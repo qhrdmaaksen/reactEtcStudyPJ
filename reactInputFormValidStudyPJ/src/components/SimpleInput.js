8f908f223dc4ec2ba1ced1ee0e916f3b02d1c004
vitamin777 <rlaalsdn8@gmail.com>
2022-08-21 22:51:32 +0900

    refactor : enteredNameTouched state 를 하나 더 둬서 enteredNameIsValid 의 state 를 false 초기 상태로 두고 활용할 수 있게해둠
    -초기 state 를 true 로 두는것은 처음부터 유효하다고 정의를 하는것이라 맞지 않다고 여겨 false 로 두기 위한 state 하나를 더 만들어 활용
    
    --기능 정상 확인 완료 커밋

--- reactInputFormValidStudyPJ/src/components/SimpleInput.js
+++ reactInputFormValidStudyPJ/src/components/SimpleInput.js
@@ -1,4 +1,4 @@
-import {useState, useRef} from 'react';
+import {useState, useRef, useEffect} from 'react';
 import '../index.css'
 const SimpleInput = (props) => {
 
@@ -7,7 +7,15 @@ const SimpleInput = (props) => {
 
 	const [enteredName, setEnteredName] = useState('')
 
-	const [enteredNameValid, setEnteredNameValid] = useState(true)
+	const [enteredNameIsValid, setEnteredNameIsValid] = useState(false)
+
+	const [enteredNameTouched, setEnteredNameTouched] = useState(false)
+
+	useEffect(()=> {
+		if(enteredNameIsValid){
+			console.log('valid')
+		}
+	}, [enteredNameIsValid])
 
 	const nameInputChangeHandler = (event) => {
 		/*event.target 이 입력 값이 되고 키 입력마다 해당 함수가 작동해 enteredName state update*/
@@ -19,13 +27,14 @@ const SimpleInput = (props) => {
 		* -*/
 		event.preventDefault()
 
+		setEnteredNameTouched(true) /*FORM 이 제출되는 순간 입력값이 사용자가 확인했기에 건드려졌다고 볼 수 있음*/
 		/*.trim()을 이용해 입력 값의 처음부터 끝 사이에 있는 공백문자를 없앨 수 있음
 		* 입력된이름의 유효성 검사*/
 		if (enteredName.trim() === ''){
-			setEnteredNameValid(false)
+			setEnteredNameIsValid(false)
 			return ; /*빈 문자열일 경우 실행되지않도록 return 하여 함수의 뒷부분을 실행되지않도록함*/
 		}
-		setEnteredNameValid(true)
+		setEnteredNameIsValid(true)
 
 		/*nameInputChangeHandler Fn 에 의해 마지막으로 저장된 name 콘솔 출력*/
 		console.log('formSubmissionHandler ::: ', enteredName)
@@ -34,13 +43,15 @@ const SimpleInput = (props) => {
 		console.log('formSubmissionHandler ref ::: ', enteredValue)
 
 		/*ref 를 사용해 초기화하는 방법은 DOM 을 직접 조작하는 것으로 바닐라 js 를 이용해 DOM 에 접근해 무언가 변경하는 것이며 보통 지양해야 하는 방법*/
-		nameInputRef.current.value=""
+		/*nameInputRef.current.value=""*/
 
 		setEnteredName('')
 	}
 
+	const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched
+
 	/*입력 값이 true/false 라면 css 의 form-control invalid 로*/
-	const nameInputClasses = enteredNameValid ? 'form-control' :'form-control invalid'
+	const nameInputClasses = nameInputIsInvalid ? 'form-control invalid' :'form-control'
 
 	return (
 			<form onSubmit={formSubmissionHandler}>
@@ -53,7 +64,7 @@ const SimpleInput = (props) => {
 							id='name'
 							onChange={nameInputChangeHandler}/>
 				</div>
-				{!enteredNameValid && <p className="error-text">이름 입력란이 비어있습니다. 이름을 입력해주세요.</p>}
+				{nameInputIsInvalid && <p className="error-text">이름 입력란이 비어있습니다. 이름을 입력해주세요.</p>}
 				<div className="form-actions">
 					<button>Submit</button>
 				</div>
