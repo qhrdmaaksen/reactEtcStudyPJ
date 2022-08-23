ea331276421f92c0019466965f87d61e1874508a
vitamin777 <rlaalsdn8@gmail.com>
2022-08-24 00:06:36 +0900

    refactor : BasicForm 에 firstName,LastName,emailAddress 입력 및 검증 기능 추가
    -커스텀 훅 useInput02 에서 useReducer 로 isTouched,enteredValue 2 개의 state 를 묶어 활용했음
    -dispatch 안에서 type 설정을 해줬으며 해당 type 에따라 조건부 반환하도록함
    
    --기능 정상 확인 완료 커밋

--- reactInputFormValidStudyPJ/src/hooks/use-input02.js
+++ reactInputFormValidStudyPJ/src/hooks/use-input02.js
@@ -1,26 +1,65 @@
-import {useState} from 'react'
+import {useReducer, useState} from 'react'
+
+/*초기 입력 상태*/
+const initialInputState = {
+	value: '',
+	isTouched: false,
+}
+
+/*reducer 에는 2 개의 인자 필요, 이전 state 와 리액트에 의해 전달되는 action
+* -action 은 code 에 전달되어 최종적으로 새로운 state 반환*/
+const inputStateReducer = (state, action) => {
+	if (action.type === 'INPUT') {
+		return {value: action.value, isTouched: state.isTouched}
+	}
+	/*focus 를 잃은 경우 사용자가 입력칸을 건드렸단 의미로 true 로 새로운 객체 설정*/
+	if (action.type === 'BLUR') {
+		return {isTouched: true, value: state.value}
+	}
+	if (action.type === 'RESET') {
+		return {isTouched: false, value: ''}
+	}
+	return {
+		value: '',
+		isTouched: false,
+	}
+}
 
 const useInput02 = (validateValue) => {
-	const [enteredValue, setEnteredValue] = useState('');
-	const [isTouched, setIsTouched] = useState(false);
+	const [inputState, dispatch] =useReducer(inputStateReducer, initialInputState)
 
-	const valueIsValid = validateValue(enteredValue);
-	const hasError = !valueIsValid && isTouched;
+	/* reducer 사용함에 따라 주석처리
+	const [enteredValue, setEnteredValue] = useState('');
+	const [isTouched, setIsTouched] = useState(false);*/
+
+	const valueIsValid = validateValue(inputState.value);
+	const hasError = !valueIsValid && inputState.isTouched;
 
 	const inputChangeHandler = (event) => {
-		setEnteredValue(event.target.value)
+		dispatch({
+			type: 'INPUT',
+			value: event.target.value,
+		})
+		/*reducer 사용함에 따라 주석처리 setEnteredValue(event.target.value)*/
 	}
 	const inputBlurHandler = () => {
-		setIsTouched(true)
+		dispatch({
+			type: 'BLUR',
+		})
+		/*reducer 사용함에 따라 주석처리 setIsTouched(true)*/
 	}
 
 	const reset = () => {
+		dispatch({
+			type: 'RESET',
+		})
+		/*reducer 사용함에 따라 주석처리
 		setEnteredValue('')
-		setIsTouched(false)
+		setIsTouched(false)*/
 	}
 
 	return {
-		value: enteredValue,
+		value: inputState.value,
 		hasError,
 		valueIsValid,
 		reset,
