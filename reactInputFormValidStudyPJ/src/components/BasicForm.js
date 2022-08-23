af0208ae206165436fa952eb9e2a0cb0205f24d4
vitamin777 <rlaalsdn8@gmail.com>
2022-08-23 22:35:59 +0900

    feat : BasicForm 에 firstName,LastName,emailAddress 입력 및 검증 기능 추가
    -커스텀 훅 useInput02 를 활용해 state value, error, isValid, reset 반환하여 사용
    -inputChangeHandler 함수로 입력 값을 가져오며 inputBlurHandler 함수는 입력란을 클릭했을 경우 아무것도 입력하지않고 건너띄면 blur 처리 되도록 함
    -reset 은 enteredValue state 초기화 및 isTouched false 로 두어 초기화하도록했음
    -error 는 validateValue 를 useInput02 의 인수로 함로 받아, 함수로 받을 시 함수 정의에 따라 유효하지않다면 hasError true 로하여 활용했음
    -BasicForm.js 에서 formIsValid 로 세가 입력이 모두 true 일때 submit 되도록함
    
    --기능 정상 확인 완료 커밋

--- reactInputFormValidStudyPJ/src/components/BasicForm.js
+++ reactInputFormValidStudyPJ/src/components/BasicForm.js
@@ -1,25 +1,99 @@
+import useInput02 from '../hooks/use-input02'
+import '../index.css'
+
+const isNotEmpty = value => value.trim() !== '';
+const isEmail = value => value.includes('@');
+
 const BasicForm = (props) => {
-  return (
-    <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
-        </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
-        </div>
-      </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
-      </div>
-      <div className='form-actions'>
-        <button>Submit</button>
-      </div>
-    </form>
-  );
+	const {
+		value: firstNameValue,
+		hasError: firstNameHasError,
+		valueIsValid: firstNameIsValid,
+		reset: resetFirstName,
+		inputChangeHandler: firstNameChangeHandler,
+		inputBlurHandler: firstNameBlurHandler,
+	} = useInput02(isNotEmpty)
+	const {
+		value: lastNameValue,
+		hasError: lastNameHasError,
+		valueIsValid: lastNameIsValid,
+		reset: resetLastName,
+		inputChangeHandler: lastNameChangeHandler,
+		inputBlurHandler: lastNameBlurHandler,
+	} = useInput02(isNotEmpty)
+	const {
+		value: emailValue,
+		hasError: emailHasError,
+		valueIsValid: emailIsValid,
+		reset: resetEmail,
+		inputChangeHandler: emailChangeHandler,
+		inputBlurHandler: emailBlurHandler,
+	} = useInput02(isEmail)
+
+	let formIsValid = false;
+	if (firstNameIsValid && lastNameIsValid && emailIsValid) {
+		formIsValid = true
+	}
+
+	const submitHandler = (event) => {
+		event.preventDefault()
+
+		if (!formIsValid) {
+			return;
+		}
+
+		console.log(' firstname ::: ', firstNameValue)
+		console.log(' lastname ::: ', lastNameValue)
+		console.log(' email ::: ', emailValue)
+
+		resetFirstName()
+		resetLastName()
+		resetEmail()
+	}
+
+	const firstNameInputClasses = firstNameHasError ? 'form-control invalid' : 'form-control';
+	const lastNameInputClasses = lastNameHasError ? 'form-control invalid' : 'form-control';
+	const emailInputClasses = emailHasError ? 'form-control invalid' : 'form-control';
+
+	return (
+			<form onSubmit={submitHandler}>
+				<div className={firstNameInputClasses}>
+					<div className='form-control'>
+						<label htmlFor='firstName'>First Name</label>
+						<input
+								type='text'
+								id='firstName'
+								value={firstNameValue}
+								onChange={firstNameChangeHandler}
+								onBlur={firstNameBlurHandler}/>
+					</div>
+					{firstNameHasError && <p className="error-text">이름을 입력해주세요.</p>}
+					<div className={lastNameInputClasses}>
+						<label htmlFor='lastName'>Last Name</label>
+						<input
+								type='text'
+								id='lastName'
+								value={lastNameValue}
+								onChange={lastNameChangeHandler}
+								onBlur={lastNameBlurHandler}/>
+					</div>
+					{lastNameHasError && <p className="error-text">이름을 입력해주세요.</p>}
+				</div>
+				<div className={emailInputClasses}>
+					<label htmlFor='email'>E-Mail Address</label>
+					<input
+							type='email'
+							id='email'
+							value={emailValue}
+							onChange={emailChangeHandler}
+							onBlur={emailBlurHandler}/>
+				</div>
+				{emailHasError && <p className="error-text">이메일을 다시 확인해주세요.</p>}
+				<div className='form-actions'>
+					<button disabled={!formIsValid}>Submit</button>
+				</div>
+			</form>
+	);
 };
 
 export default BasicForm;
