8256542948e8bc5c8d5b6592751905a2c3a07714
vitamin777 <rlaalsdn8@gmail.com>
2022-09-26 03:30:45 +0900

    style : 토글 버튼 클릭시 불투명 효과로 사라지게함
    -Transition component 로 App.js 에서 div 를 wrapping 하여 랩핑된 요소들을 컨트롤 할 수 있도록하였음
    -Transition attr 로 in attr 은 showBlock 에대한 state 에 대해 반환받으며 toggle button click 시 반환되는 state 를 받음
    timeout attr 을 추가해 1 초 의 시간을 두고 동적으로 렌더링 하도록함, mountOnEnter 와 unmountOnExit 를 속성으로 두고 DOM 에 요소 추가 및 제거 토록 함
    -div box 는 transition 의 state 에따라
    opacity 를 둬서 불투명에대한 관리토록함
    transition attr 에 불투명 1초 서서히 감소토록함
    
    -기능 및 출력 정상 확인

--- reactAnimationDemo/src/App.js
+++ reactAnimationDemo/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import Transition from 'react-transition-group/Transition';
 import './App.css';
 import Modal from './components/Modal/Modal';
 import Backdrop from './components/Backdrop/Backdrop';
@@ -8,6 +8,7 @@ import List from './components/List/List';
 class App extends Component {
   state = {
     modalIsOpen: false,
+    showBlock: false,
   };
   showModal = () => {
     this.setState({ modalIsOpen: true });
@@ -19,8 +20,40 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
-        <Backdrop show={this.state.modalIsOpen} />
+        <button
+          className="Button"
+          onClick={() => this.setState((prevState) => ({ showBlock: !prevState.showBlock }))}
+        >
+          Toggle
+        </button>
+        <br />
+        {/*in attr 은 transition compo wrapping 된 요소를 보일지 말지 결정*/}
+        <Transition
+            in={this.state.showBlock}
+            timeout={1000}
+            mountOnEnter  /*단순 불리언 prop*/
+            unmountOnExit /*만약 in attr 이 true 라면 DOM 에 wrap 요소를 추가하고 반대로 unmountOnExit 추가하면
+            DOM 에서 완전히 제거됨*/
+        >
+          {/*Transition 컴포넌트의 자식으로 전달하는 이 함수 안의 state*/}
+          {(state) => (
+            <div
+              style={{
+                backgroundColor: 'red',
+                width: 100,
+                height: 100,
+                margin: 'auto',
+                transition:
+                  'opacity 1s ease-out' /*1초 동안 불투명되며 state 에 따라 출력되거나 사라짐*/,
+                opacity: state === 'exiting' ? 0 : 1, /*exited 로하면 바로 사라짐 exiting 으로하면 천천히 사라짐*/
+              }}
+            ></div>
+          )}
+        </Transition>
+        {this.state.modalIsOpen ? (
+          <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
+        ) : null}
+        {this.state.modalIsOpen ? <Backdrop show={this.state.modalIsOpen} /> : null}
         <button className="Button" onClick={this.showModal}>
           Open Modal
         </button>
