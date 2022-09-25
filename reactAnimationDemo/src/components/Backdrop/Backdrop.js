63223bae35ad55be33ae63659d1e459a264af934
vitamin777 <rlaalsdn8@gmail.com>
2022-09-25 19:16:00 +0900

    feat : modal open & close function add
    -Modal.js Backdrop.js 에서 cssClass 상수에 props.show 로 true, false 에 따라 join method 로 문자열로 연결하여 화면 표시제어하도록함
    -App.js 에서 props show 의 state 가 결정될 state modalIsOpen 객체를 생성하여 show modal or close modal 에따라서 props.show 에 state value 가 결정되도록함
    
    style : Backdrop.css , Modal.css class add
    
    -open close 로 표시 비표시 display : none or block setting
    
    -기능 및 출력 정상 확인

--- reactAnimationDemo/src/components/Backdrop/Backdrop.js
+++ reactAnimationDemo/src/components/Backdrop/Backdrop.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 import './Backdrop.css';
 
-const backdrop = (props) => (
-    <div className="Backdrop"></div>
-);
+const backdrop = (props) => {
+  /*어떤 props 를 받느냐에따라 open close 결정*/
+  const cssClasses = ['Backdrop', props.show ? 'BackdropOpen' : 'BackdropClose'];
+  /*결과에따라 cssClasses 에있는 문자열을 붙이도록함 join 에 공백을 둬야함*/
+  return <div className={cssClasses.join(' ')}></div>;
+};
 
 export default backdrop;
--- reactAnimationDemo/src/components/Modal/Modal.js
+++ reactAnimationDemo/src/components/Modal/Modal.js
@@ -2,11 +2,18 @@ import React from 'react';
 
 import './Modal.css';
 
-const modal = (props) => (
-    <div className="Modal">
-        <h1>A Modal</h1>
-        <button className="Button" onClick={props.closed}>Dismiss</button>
+const modal = (props) => {
+  /*어떤 props 를 받느냐에따라 open close 결정*/
+  const cssClasses = ['Modal', props.show ? 'ModalOpen' : 'ModalClose'];
+  return (
+    /*결과에따라 cssClasses 에있는 문자열을 붙이도록함 join 에 공백을 둬야함*/
+    <div className={cssClasses.join(' ')}>
+      <h1>A Modal</h1>
+      <button className="Button" onClick={props.closed}>
+        Dismiss
+      </button>
     </div>
-);
+  );
+};
 
 export default modal;
